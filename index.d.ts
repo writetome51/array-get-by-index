1dc4e08fafbc85b7b8155067c1aa42bc3768a926
Steve Thompson <writetome51@gmail.com>
2021-04-19 00:12:10 -0600

    Added Typescript declaration. Improved speed. Upped version to 2.0.1

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function getByIndex<T>(index: number, array: T[]): T;
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import {errorIfIndexNotValid} from 'error-if-index-not-valid';
 // index can be negative or positive.
 
 export function getByIndex(index, array) {
-	errorIfIndexNotValid(index, array.length); // Validates both index and array.
+	const length = array.length;
+	errorIfIndexNotValid(index, length); // Validates both index and array.
 
-	if (index < 0) return array[array.length + index];
+	if (index < 0) return array[length + index];
 	return array[index];
 }
